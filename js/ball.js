07ef547eddaf8bf8d0c0ee230a31d896d6916189
Evan Xd <itoyxd@gmail.com>
2015-05-17 20:13:33 +0800

    Fix bug: Update logic of how to move the ball

--- js/ball.js
+++ js/ball.js
@@ -15,22 +15,11 @@
   Ball.prototype.constructor = Ball;
 
   Ball.prototype._howToMove = function(isCollided) {
-    var matrix = this._canvas.matrix;
-    var x = this._x;
-    var y = this._y;
-    if ((matrix[x - 1] && matrix[x - 1][y - 1]) ||
-        (matrix[x + 1] && matrix[x + 1][y - 1]) ||
-        (matrix[x - 1] && matrix[x - 1][y + 1]) ||
-        (matrix[x + 1] && matrix[x + 1][y + 1])) {
-      this._direction.x = -this._direction.x;
-      this._direction.y = -this._direction.y;
-    } else {
-      ['x', 'y'].forEach((dim) => {
-        if (isCollided[dim]) {
-          this._direction[dim] = -this._direction[dim];
-        }
-      });
-    }
+    ['x', 'y'].forEach((dim) => {
+      if (isCollided[dim]) {
+        this._direction[dim] = -this._direction[dim];
+      }
+    });
   };
 
   exports.Ball = Ball;
