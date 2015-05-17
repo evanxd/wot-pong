dbcc0829b637954fd9d906d29df818e5601bc8d7
Evan Xd <itoyxd@gmail.com>
2015-05-17 10:38:45 +0800

    Export ledMatrix as public

--- js/index.js
+++ js/index.js
@@ -2,18 +2,24 @@
 /* global Pong */
 'use strict';
 
-(function() {
+(function(exports) {
   var ledMatrix = new LedMatrixHelper('e1:09:43:ea:dd:68');
   var startButton = document.querySelector('#start');
   var pong = new Pong(ledMatrix);
 
-  window.addEventListener('bluetoothready', function() {
-    startButton.addEventListener('click', function() {
-      if (pong.isPaused) {
-        pong.start();
-      } else {
-        pong.pause();
-      }
-    });
+  startButton.addEventListener('click', function() {
+    if (pong.isPaused) {
+      pong.start();
+    } else {
+      pong.pause();
+    }
   });
-}());
+
+  window.addEventListener('bluetoothready', function bluetoothreadyHandler() {
+    window.removeEventListener('bluetoothready', bluetoothreadyHandler);
+    startButton.disabled = false;
+  });
+
+  // For debugging.
+  exports.ledMatrix = ledMatrix;
+}(window));
