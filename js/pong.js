4056a26388125df7d836dbc6f4e8d1615dd55b24
Evan Xd <itoyxd@gmail.com>
2015-05-20 10:27:12 +0800

    Refactor: init method in Pong module

--- js/pong.js
+++ js/pong.js
@@ -14,27 +14,7 @@
     this._ball = new Ball(canvas, this._timer);
     this._paddle1 = new Paddle(canvas, this._timer);
     // this._paddle2 = new Paddle(canvas);
-    this._timer.addAction(function() {
-      var timer = this._timer;
-      var speed = this._timer.speed;
-      this._checkGameover();
-      if (this._isBallHit()) {
-        new Audio('resources/sounds/hit-ball.ogg').play();
-        navigator.vibrate([150]);
-        // Ball speed will be faster and faster
-        // after player hits ball more and more times.
-        if (speed >= 100) {
-          timer.speed = speed - 20;
-          timer.pause();
-          timer.start();
-        }
-      }
-    }.bind(this));
-    this._ball.move();
-    this._paddle1.control({
-      left: 'move-paddle-left',
-      right: 'move-paddle-right',
-    });
+    this._init();
   }
 
   Pong.prototype = {
@@ -50,18 +30,34 @@
     },
 
     _init: function() {
-      if (this._initilized) {
-        return;
-      }
+      this._timer.addAction(function() {
+        var timer = this._timer;
+        var speed = this._timer.speed;
+        this._checkGameover();
+        if (this._isBallHit()) {
+          new Audio('resources/sounds/hit-ball.ogg').play();
+          navigator.vibrate([150]);
+          // Ball speed will be faster and faster
+          // after player hits ball more and more times.
+          if (speed >= 100) {
+            timer.speed = speed - 20;
+            timer.pause();
+            timer.start();
+          }
+        }
+      }.bind(this));
+      this._ball.move();
+      this._paddle1.control({
+        left: 'move-paddle-left',
+        right: 'move-paddle-right',
+      });
+    },
+
+    start: function() {
       this._ball.draw(Math.floor(Math.random() * 6), 1);
       this._paddle1.draw(Math.floor(Math.random() * 5), 7);
       // this._paddle2.draw(5, 0);
       this._timer.speed = DEFAULT_SPEED;
-      this._initilized = true;
-    },
-
-    start: function() {
-      this._init();
       this._timer.start();
     },
 
@@ -74,7 +70,6 @@
         this._timer.pause();
         new Audio('resources/sounds/game-over.ogg').play();
         navigator.vibrate([3000]);
-        this._initilized = false;
         console.log('Game Over!');
       }
     },
