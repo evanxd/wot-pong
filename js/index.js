a5d7010f769fb729142aab761cc6f0f6f6f12b24
Evan Xd <itoyxd@gmail.com>
2015-05-21 21:16:59 +0800

    Add new button to start a new game

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
   var pong = new Pong(ledMatrix);
   var reconnectButton = document.querySelector('#reconnect');
   var demoButton = document.querySelector('#demo');
-  var startButton = document.querySelector('#start');
+  var newButton = document.querySelector('#new');
   var leftButton = document.querySelector('#left');
   var rightButton = document.querySelector('#right');
   var message = document.querySelector('#message');
@@ -33,19 +33,16 @@
     pong.start({ isDemoMode: true });
   });
 
-  startButton.addEventListener('click', function() {
-    if (pong.isPaused) {
-      pong.start();
-    } else {
-      pong.pause();
-    }
+  newButton.addEventListener('click', function() {
+    !pong.isPaused && pong.pause();
+    pong.start();
   });
 
   message.innerHTML = 'Pixel is connecting...';
   window.addEventListener('bluetoothready', function bluetoothreadyHandler() {
     window.removeEventListener('bluetoothready', bluetoothreadyHandler);
     message.innerHTML = 'Pixel is connected.';
-    startButton.disabled = false;
+    newButton.disabled = false;
   });
 
   // Need to disconnect BLE before app is closed,
